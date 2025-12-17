4e4f41cac2a8608a1d386778560f476a88d50117
Luca Britten <lucabritten17@icloud.com>
2025-12-17 19:38:32 +0100

    Set up minimal Maven project with executable main class

---
+++ f1insight/src/main/java/htwsaar/nordpol/App.java
@@ -0,0 +1,10 @@
+package htwsaar.nordpol;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from F1 Insight tool!");
+    }
+}
