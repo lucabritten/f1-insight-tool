5a23098b2de88d8bcbf42da70e2113e706de3f10
ginakessler <ginarose.ksslr@gmail.com>
2026-01-13 13:36:50 +0100

    Testing my rights because nothing works

--- f1insight/src/main/java/htwsaar/nordpol/App.java
+++ f1insight/src/main/java/htwsaar/nordpol/App.java
@@ -1,7 +1,7 @@
 package htwsaar.nordpol;
 
 import htwsaar.nordpol.API.DriverClient;
-// Test
+// Test 2
 public class App {
     public static void main(String[] args) {
         DriverClient client = new DriverClient();
