8e1e0c9a7da5e06828d3781acb7f564b6e45595c
Jona Mees <jonamees03@gmail.com>
2026-01-13 12:40:09 +0100

    added driver domain record

---
+++ f1insight/src/main/java/htwsaar/nordpol/Domain/Driver.java
@@ -0,0 +1,9 @@
+package htwsaar.nordpol.Domain;
+
+public record Driver(String surname,
+                     String lastName,
+                     int driverNumber,
+                     String countryCode,
+                     String teamName) {
+
+}
