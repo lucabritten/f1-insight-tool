2bdf42fbbfe3e22923c173fee7f839b3a0b9c23d
Jona Mees <jonamees03@gmail.com>
2026-01-14 12:26:45 +0100

    Initial Driver table schema and basic JooqDriverRepo implementation

--- f1insight/src/main/java/htwsaar/nordpol/Repository/JooqDriverRepo.java
+++ f1insight/src/main/java/htwsaar/nordpol/Repository/JooqDriverRepo.java
@@ -1,4 +1,36 @@
 package htwsaar.nordpol.Repository;
 
-public class JooqDriverRepo {
+import htwsaar.nordpol.API.DTO.DriverApiDto;
+import org.jooq.DSLContext;
+
+import static com.nordpol.jooq.tables.Drivers.*;
+import static org.jooq.impl.DSL.asterisk;
+
+public class JooqDriverRepo implements DriverRepo{
+
+    private final DSLContext create;
+
+    public JooqDriverRepo(DSLContext create) {
+        this.create = create;
+    }
+
+    @Override
+    public void saveDriver(DriverApiDto driverApiDto) {
+        create
+                .insertInto(DRIVERS)
+                .values(driverApiDto.driver_number(),
+                        driverApiDto.full_name(),
+                        driverApiDto.country_code()
+                )
+                .execute();
+    }
+
+    @Override
+    public DriverApiDto getDriverByFullname(String surname, String lastName) {
+        return create
+                .select(asterisk())
+                .from(DRIVERS)
+                .where(DRIVERS.FULL_NAME.eq(surname + " " + lastName))
+                .fetchOneInto(DriverApiDto.class);
+    }
 }
