09615d6d114a356f6cd70188b765a9ac19b7c0c1
STRE4MY <niklas.belieres@gmail.com>
2026-01-14 15:58:32 +0100

    added in-memory jOOQ repo tests for DriverRepo

--- f1insight/src/main/java/htwsaar/nordpol/Repository/JooqDriverRepo.java
+++ f1insight/src/main/java/htwsaar/nordpol/Repository/JooqDriverRepo.java
@@ -4,7 +4,6 @@ import htwsaar.nordpol.API.DTO.DriverApiDto;
 import org.jooq.DSLContext;
 
 import static com.nordpol.jooq.tables.Drivers.*;
-import static org.jooq.impl.DSL.asterisk;
 
 public class JooqDriverRepo implements DriverRepo{
 
