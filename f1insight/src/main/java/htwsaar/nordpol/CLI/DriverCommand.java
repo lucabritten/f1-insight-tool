a0f2892cd4e7036f9ae023388bd0083d60649b87
ginakessler <ginarose.ksslr@gmail.com>
2026-01-18 09:59:09 +0100

    Added basic implementation of base command and driver command

--- f1insight/src/main/java/htwsaar/nordpol/CLI/DriverCommand.java
+++ f1insight/src/main/java/htwsaar/nordpol/CLI/DriverCommand.java
@@ -1,4 +1,34 @@
 package htwsaar.nordpol.CLI;
 
-public class DriverCommand {
+import htwsaar.nordpol.Service.DriverService;
+import htwsaar.nordpol.config.ApplicationContext;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "Driver-Info")
+
+public class DriverCommand implements Runnable {
+
+    @Option(names = {"--firstName",
+            "-fn"},
+            //description
+            required = true
+    )
+    private String firstName;
+
+    @Option(names = {"--lastName",
+            "-ln"},
+            //description
+            required = true
+    )
+    private String lastName;
+
+    private final DriverService driverService = ApplicationContext.driverService();
+
+    @Override
+    public void run() {
+        System.out.println(driverService.getDriverByName(firstName, lastName));
+    }
 }
--- f1insight/src/main/java/htwsaar/nordpol/CLI/F1CLI.java
+++ f1insight/src/main/java/htwsaar/nordpol/CLI/F1CLI.java
@@ -1,4 +1,14 @@
 package htwsaar.nordpol.CLI;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "F1Insight",
+        mixinStandardHelpOptions = true,
+        subcommands = {DriverCommand.class}
+)
 public class F1CLI {
+
 }
