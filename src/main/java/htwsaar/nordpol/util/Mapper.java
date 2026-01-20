4d9a2f8a44f63bc38f8d5157152bc8765d96b445
Jona Mees <jonamees03@gmail.com>
2026-01-20 13:12:23 +0100

    added meeting mapping method

--- src/main/java/htwsaar/nordpol/util/Mapper.java
+++ src/main/java/htwsaar/nordpol/util/Mapper.java
@@ -1,7 +1,9 @@
 package htwsaar.nordpol.util;
 
 import htwsaar.nordpol.api.dto.DriverDto;
+import htwsaar.nordpol.api.dto.MeetingDto;
 import htwsaar.nordpol.domain.Driver;
+import htwsaar.nordpol.domain.Meeting;
 
 public class Mapper {
 
@@ -18,4 +20,14 @@ public class Mapper {
                 dto.country_code()
         );
     }
+    public static Meeting toMeeting(MeetingDto dto){
+        return new Meeting(
+                dto.meeting_key(),
+                dto.country_code(),
+                dto.country_name(),
+                dto.location(),
+                dto.year()
+        );
+    }
+
 }
