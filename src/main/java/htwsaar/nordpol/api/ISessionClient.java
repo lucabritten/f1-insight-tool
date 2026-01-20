29221358f554bf40914fe13e444e8d01e045970c
ginakessler <ginarose.ksslr@gmail.com>
2026-01-20 12:49:55 +0100

    Added missing imports

--- src/main/java/htwsaar/nordpol/api/ISessionClient.java
+++ src/main/java/htwsaar/nordpol/api/ISessionClient.java
@@ -1,5 +1,9 @@
 package htwsaar.nordpol.api;
 
+import htwsaar.nordpol.api.dto.SessionDto;
+
+import java.util.Optional;
+
 public interface ISessionClient {
 
     Optional<SessionDto> getSessionByMeetingKeyAndSessionType(int meetingKey, String sessionType);
