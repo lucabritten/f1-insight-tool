48fae88444b9543515799619e9606876fb2d0dfa
Jona Mees <jonamees03@gmail.com>
2026-01-21 09:37:37 +0100

    added IWeatherClient Interface

---
+++ src/main/java/htwsaar/nordpol/api/weather/IWeatherClient.java
@@ -0,0 +1,10 @@
+package htwsaar.nordpol.api.weather;
+
+import htwsaar.nordpol.api.dto.WeatherDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IWeatherClient {
+    Optional<List<WeatherDto>> getWeatherDataByMeetingKeyAndSessionKey(int meetingKey, int sessionKey);
+}
