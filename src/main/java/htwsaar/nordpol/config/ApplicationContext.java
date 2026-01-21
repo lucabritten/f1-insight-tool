d36f44b5b019f3bf78637698c4d03131e3b083d0
Luca Britten <lucabritten17@icloud.com>
2026-01-21 18:47:57 +0100

    Update `WeatherClient` URL path to use weather endpoint, extend `ApplicationContext` to provide `MeetingService` and `SessionService`, and adjust `WeatherService` initialization accordingly.

--- src/main/java/htwsaar/nordpol/config/ApplicationContext.java
+++ src/main/java/htwsaar/nordpol/config/ApplicationContext.java
@@ -1,12 +1,20 @@
 package htwsaar.nordpol.config;
 
 import htwsaar.nordpol.api.driver.DriverClient;
+import htwsaar.nordpol.api.meeting.MeetingClient;
+import htwsaar.nordpol.api.session.SessionClient;
 import htwsaar.nordpol.api.weather.WeatherClient;
 import htwsaar.nordpol.repository.driver.IDriverRepo;
 import htwsaar.nordpol.repository.driver.JooqDriverRepo;
+import htwsaar.nordpol.repository.meeting.IMeetingRepo;
+import htwsaar.nordpol.repository.meeting.JooqMeetingRepo;
+import htwsaar.nordpol.repository.session.ISessionRepo;
+import htwsaar.nordpol.repository.session.JooqSessionRepo;
 import htwsaar.nordpol.repository.weather.IWeatherRepo;
 import htwsaar.nordpol.repository.weather.JooqWeatherRepo;
 import htwsaar.nordpol.service.DriverService;
+import htwsaar.nordpol.service.MeetingService;
+import htwsaar.nordpol.service.SessionService;
 import htwsaar.nordpol.service.WeatherService;
 
 /**
@@ -31,15 +39,27 @@ public class ApplicationContext {
      *
      * @return a fully initialized DriverService
      */
-    public static DriverService driverService(){
+    public static DriverService driverService() {
         IDriverRepo IDriverRepo = new JooqDriverRepo(JooqConfig.createContext());
         DriverClient driverClient = new DriverClient();
         return new DriverService(IDriverRepo, driverClient);
     }
 
+    public static MeetingService meetingService() {
+        IMeetingRepo IMeetingRepo = new JooqMeetingRepo(JooqConfig.createContext());
+        MeetingClient meetingClient = new MeetingClient();
+        return new MeetingService(IMeetingRepo, meetingClient);
+    }
+
+    public static SessionService sessionService() {
+        ISessionRepo ISessionRepo = new JooqSessionRepo(JooqConfig.createContext());
+        SessionClient sessionClient = new SessionClient();
+        return new SessionService(ISessionRepo, sessionClient);
+    }
+
     public static WeatherService weatherService() {
         IWeatherRepo weatherRepo = new JooqWeatherRepo(JooqConfig.createContext());
         WeatherClient weatherClient = new WeatherClient();
-        return new WeatherService(weatherClient, weatherRepo);
+        return new WeatherService(weatherClient, weatherRepo, sessionService(),meetingService());
     }
 }
