eb2d70bd574dc38ec372c41c7be8bdc43b0cfeff
Jona Mees <jonamees03@gmail.com>
2026-01-24 12:06:06 +0100

    added LapCommand

---
+++ src/main/java/htwsaar/nordpol/cli/LapCommand.java
@@ -0,0 +1,59 @@
+package htwsaar.nordpol.cli;
+import htwsaar.nordpol.cli.view.LapsWithContext;
+import htwsaar.nordpol.domain.SessionName;
+import htwsaar.nordpol.service.LapService;
+import htwsaar.nordpol.util.Formatter;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "lap-info",
+        description = "Print lap infos",
+        mixinStandardHelpOptions = true
+)
+
+public class LapCommand implements Runnable {
+
+    @Option(names = {"--location", "-l"},
+            description = "The meeting location (e.g., \"monza\")",
+            required = true)
+
+    private String location;
+
+    @Option(names = {"--year", "-y"},
+            description = "The season year (e.g., 2024)",
+            defaultValue = "2024"
+    )
+    private int year;
+
+    @Option(names = {"--sessionName", "-sn"},
+            description = "The session name (e.g., PRACTICE_1, QUALIFYING, RACE)",
+            required = true)
+
+    private SessionName sessionName;
+
+    @Option(names = {"--driverNumber", "-d"},
+            description = "The driver number (e.g., 44)",
+            required = true)
+
+    private int driverNumber;
+
+
+    private final LapService lapService;
+
+    public LapCommand(LapService lapService) {
+        this.lapService = lapService;
+    }
+
+    @Override
+    public void run() {
+        try {
+            LapsWithContext lap = lapService.getLapsByLocationYearSessionNameAndDriverNumber(location, year, sessionName, driverNumber);
+            String output = Formatter.formatLaps(lap);
+            System.out.println(output);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
