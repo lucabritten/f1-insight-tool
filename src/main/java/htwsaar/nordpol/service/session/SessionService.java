bc5e7352b3a1c7d1fc7649f27556c52d65ee0e81
Jona Mees <jonamees03@gmail.com>
2026-01-27 20:16:13 +0100

    Fix session API lookup to use SessionName.apiValue()

--- src/main/java/htwsaar/nordpol/service/session/SessionService.java
+++ src/main/java/htwsaar/nordpol/service/session/SessionService.java
@@ -6,41 +6,40 @@ import htwsaar.nordpol.domain.Session;
 import htwsaar.nordpol.domain.SessionName;
 import htwsaar.nordpol.exception.SessionNotFoundException;
 import htwsaar.nordpol.repository.session.ISessionRepo;
-import htwsaar.nordpol.service.meeting.IMeetingService;
 import htwsaar.nordpol.util.Mapper;
 
 import java.util.Optional;
 
 public class SessionService implements ISessionService {
 
-        private final ISessionRepo sessionRepo;
-        private final ISessionClient sessionClient;
+    private final ISessionRepo sessionRepo;
+    private final ISessionClient sessionClient;
 
-        public SessionService(ISessionRepo sessionRepo, ISessionClient sessionClient) {
-            if (sessionRepo == null) {
-                throw new IllegalArgumentException("sessionRepo must not be null.");
-            }
-            if (sessionClient == null) {
-                throw new IllegalArgumentException("sessionClient must not be null.");
-            }
-            this.sessionRepo = sessionRepo;
-            this.sessionClient = sessionClient;
+    public SessionService(ISessionRepo sessionRepo, ISessionClient sessionClient) {
+        if (sessionRepo == null) {
+            throw new IllegalArgumentException("sessionRepo must not be null.");
+        }
+        if (sessionClient == null) {
+            throw new IllegalArgumentException("sessionClient must not be null.");
+        }
+        this.sessionRepo = sessionRepo;
+        this.sessionClient = sessionClient;
+    }
+
+    public Session getSessionByMeetingKeyAndSessionName(int meetingKey, SessionName sessionName){
+        Optional<SessionDto> dtoFromDB = sessionRepo.getSessionByMeetingKeyAndSessionName(meetingKey, sessionName.dbValue());
+        if (dtoFromDB.isPresent()) {
+            return Mapper.toSession(dtoFromDB.get());
         }
 
-        public Session getSessionByMeetingKeyAndSessionName(int meetingKey, SessionName sessionName){
-            Optional<SessionDto> dtoFromDB = sessionRepo.getSessionByMeetingKeyAndSessionName(meetingKey, sessionName.dbValue());
-            if (dtoFromDB.isPresent()) {
-                return Mapper.toSession(dtoFromDB.get());
-            }
+        Optional<SessionDto> dtoFromApi =
+                sessionClient.getSessionByMeetingKeyAndsessionName(meetingKey, sessionName.displayName());
 
-            Optional<SessionDto> dtoFromApi =
-                    sessionClient.getSessionByMeetingKeyAndsessionName(meetingKey, sessionName.apiValue());
-
-            if (dtoFromApi.isPresent()) {
-                SessionDto sessionDto = dtoFromApi.get();
-                sessionRepo.save(sessionDto);
-                return Mapper.toSession(sessionDto);
-            }
-            throw new SessionNotFoundException(meetingKey, sessionName.toString());
+        if (dtoFromApi.isPresent()) {
+            SessionDto sessionDto = dtoFromApi.get();
+            sessionRepo.save(sessionDto);
+            return Mapper.toSession(sessionDto);
         }
+        throw new SessionNotFoundException(meetingKey, sessionName.toString());
+    }
 }
