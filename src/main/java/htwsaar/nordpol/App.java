0e63680391cf8eedaa155a6a873609aa780e0b78
Bélières <95937502+STRE4MY@users.noreply.github.com>
2026-01-27 12:27:50 +0100

    Fixed missing run in App

--- src/main/java/htwsaar/nordpol/App.java
+++ src/main/java/htwsaar/nordpol/App.java
@@ -1,10 +1,12 @@
 package htwsaar.nordpol;
 
 import htwsaar.nordpol.cli.F1CLI;
+import htwsaar.nordpol.config.DatabaseInitializer;
 import picocli.CommandLine;
 
 public class App {
     public static void main(String[] args) {
+        new DatabaseInitializer().run();
         CommandLine commandLine = new CommandLine(new F1CLI());
         int exitCode = commandLine.execute(args);
         System.exit(exitCode);
