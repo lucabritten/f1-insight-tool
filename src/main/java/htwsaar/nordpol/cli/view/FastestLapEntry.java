4a39bb6815a80ce7c2883148f0e8966b7e634275
Luca Britten <lucabritten17@icloud.com>
2026-01-29 20:24:09 +0100

    Remove unused `FastestLapEntry` record class from CLI view

--- src/main/java/htwsaar/nordpol/cli/view/FastestLapEntry.java
+++
@@ -1,9 +0,0 @@
-package htwsaar.nordpol.cli.view;
-
-public record FastestLapEntry(
-        String driverName,
-        double lapDuration,
-        int lapNumber,
-        int driverNumber
-) {
-}
