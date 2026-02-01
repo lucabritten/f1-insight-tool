170da6c147bdb15a4321576335e13f383a460f2d
Luca Britten <lucabritten17@icloud.com>
2026-02-01 17:32:22 +0100

    Additional documentation for SessionResultService

--- src/main/java/htwsaar/nordpol/service/sessionResult/ISessionResultService.java
+++ src/main/java/htwsaar/nordpol/service/sessionResult/ISessionResultService.java
@@ -2,7 +2,14 @@ package htwsaar.nordpol.service.sessionResult;
 
 import htwsaar.nordpol.cli.view.SessionResultWithContext;
 import htwsaar.nordpol.domain.SessionName;
+import htwsaar.nordpol.exception.SessionNotFoundException;
 
 public interface ISessionResultService {
+    /**
+     * Returns the session results for a given location, year and session.
+     * Results are sorted by classification rules (DNF/DNS/DSQ last, then position)
+     *
+     * @throws SessionNotFoundException if no results are available
+     */
     SessionResultWithContext getResultByLocationYearAndSessionType(String location, int year, SessionName sessionName);
 }
