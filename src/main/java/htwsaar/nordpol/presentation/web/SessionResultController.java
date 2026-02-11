25efa69d58fa548eae95d6d6144029d70c1f1dc2
Luca Britten <lucabritten17@icloud.com>
2026-02-11 20:46:02 +0100

    Feature: Added SessionResultController to fetch results to front-end

---
+++ src/main/java/htwsaar/nordpol/presentation/web/SessionResultController.java
@@ -0,0 +1,28 @@
+package htwsaar.nordpol.presentation.web;
+
+import htwsaar.nordpol.domain.SessionName;
+import htwsaar.nordpol.presentation.view.SessionResultWithContext;
+import htwsaar.nordpol.service.sessionResult.ISessionResultService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/session-result")
+public class SessionResultController {
+
+    private final ISessionResultService sessionResultService;
+
+    public SessionResultController(ISessionResultService sessionResultService) {
+        this.sessionResultService = sessionResultService;
+    }
+
+
+    public SessionResultWithContext getSessionResultsBySession(
+            @RequestParam(name = "location") String location,
+            @RequestParam(name = "year") int year,
+            @RequestParam(name = "session") String session
+    ) {
+        SessionName sessionName = SessionName.fromString(session);
+        return sessionResultService.getResultByLocationYearAndSessionType(location,year,sessionName);
+    }
+}
