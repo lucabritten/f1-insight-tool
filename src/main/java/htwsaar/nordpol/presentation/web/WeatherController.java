1d98fc03f1889ccda7182886715e58ccdfaf2776
Luca Britten <lucabritten17@icloud.com>
2026-02-14 16:42:02 +0100

    Feature: Introduced new weather controller to send weather data to frontend

---
+++ src/main/java/htwsaar/nordpol/presentation/web/WeatherController.java
@@ -0,0 +1,28 @@
+package htwsaar.nordpol.presentation.web;
+
+import htwsaar.nordpol.domain.SessionName;
+import htwsaar.nordpol.presentation.view.WeatherWithContext;
+import htwsaar.nordpol.service.weather.IWeatherService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/weather")
+@CrossOrigin(origins = "*")
+public class WeatherController {
+
+    private final IWeatherService weatherService;
+
+    public WeatherController(IWeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping
+    public WeatherWithContext getWeatherByLocationYearAndSessionName(
+            @RequestParam(name = "location") String location,
+            @RequestParam(name = "year") int year,
+            @RequestParam(name = "session") String session
+    ) {
+        SessionName sessionName = SessionName.fromString(session);
+        return weatherService.getWeatherByLocationYearAndSessionName(location, year, sessionName);
+    }
+}
