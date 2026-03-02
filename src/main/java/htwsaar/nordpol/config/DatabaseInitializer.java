9e6f174e02c8b08ed74f131ed1810a8eeaf2e6ba
Luca Britten <lucabritten17@icloud.com>
2026-03-02 08:58:19 +0100

    Change to correct db default path

--- src/main/java/htwsaar/nordpol/config/DatabaseInitializer.java
+++ src/main/java/htwsaar/nordpol/config/DatabaseInitializer.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public final class DatabaseInitializer {
 
     public void run() {
-        String dbPath = System.getenv().getOrDefault("F1_DB_PATH", "f1data.db");
+        String dbPath = System.getenv().getOrDefault("F1_DB_PATH", "/tmp/f1data.db");
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath)) {
             if (JooqConfig.schemaNeedsInitialization(connection)) {
                 JooqConfig.initializeSchema(connection);
